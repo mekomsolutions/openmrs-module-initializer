1b9346b5dd7cc2c11f7732e949fe49a3b8705f03
rbuisson <romain@mekomsolutions.com>
2017-11-16 09:40:12 +0100

    MRSCMNS-64: Idgen 'prefix' and 'suffix' fields to default to empty string
    	modified:   api/src/main/java/org/openmrs/module/initializer/api/idgen/SequentialIdentifierGeneratorLineProcessor.java

--- api/src/main/java/org/openmrs/module/initializer/api/idgen/SequentialIdentifierGeneratorLineProcessor.java
+++ api/src/main/java/org/openmrs/module/initializer/api/idgen/SequentialIdentifierGeneratorLineProcessor.java
@@ -21,8 +21,8 @@ public class SequentialIdentifierGeneratorLineProcessor extends BaseIdentifierSo
 		
 		((SequentialIdentifierGenerator) instance.getIdentifierSource()).setFirstIdentifierBase(line
 		        .get(HEADER_FIRST_ID_BASE));
-		((SequentialIdentifierGenerator) instance.getIdentifierSource()).setPrefix(line.get(HEADER_PREFIX));
-		((SequentialIdentifierGenerator) instance.getIdentifierSource()).setSuffix(line.get(HEADER_SUFFIX));
+		((SequentialIdentifierGenerator) instance.getIdentifierSource()).setPrefix(line.getString(HEADER_PREFIX, ""));
+		((SequentialIdentifierGenerator) instance.getIdentifierSource()).setSuffix(line.getString(HEADER_SUFFIX, ""));
 		((SequentialIdentifierGenerator) instance.getIdentifierSource()).setMinLength(line.getInt(HEADER_MIN_LENGTH));
 		((SequentialIdentifierGenerator) instance.getIdentifierSource()).setMaxLength(line.getInt(HEADER_MAX_LENGTH));
 		((SequentialIdentifierGenerator) instance.getIdentifierSource()).setBaseCharacterSet(line.get(HEADER_BASE_CHAR_SET));
