41d67d1b286c2396f45c5bf99af70a76e40df94c
mks-d <dimitri@mekomsolutions.com>
2019-04-25 21:53:06 +0200

    OPENMRS-74: Simplifying code + README.
      modified:   README.md
      modified:   api/src/main/java/org/openmrs/module/initializer/api/privileges/PrivilegeLineProcessor.java
      modified:   api/src/test/java/org/openmrs/module/initializer/api/PrivilegesLoaderIntegrationTest.java
      modified:   api/src/test/resources/testAppDataDir/configuration/privileges/privileges.csv
      new file:   readme/priv.md

--- api/src/main/java/org/openmrs/module/initializer/api/privileges/PrivilegeLineProcessor.java
+++ api/src/main/java/org/openmrs/module/initializer/api/privileges/PrivilegeLineProcessor.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.initializer.api.privileges;
 
-import org.apache.commons.lang3.StringUtils;
 import org.openmrs.Privilege;
 import org.openmrs.api.UserService;
 import org.openmrs.module.initializer.api.BaseLineProcessor;
@@ -16,42 +15,25 @@ public class PrivilegeLineProcessor extends BaseLineProcessor<Privilege, UserSer
 	
 	@Override
 	protected Privilege bootstrap(CsvLine line) throws IllegalArgumentException {
-		String uuid = getUuid(line.asLine());
-		Privilege privilege = service.getPrivilegeByUuid(uuid);
+		String privilegeName = line.get(HEADER_PRIVILEGE_NAME, true);
 		
-		if (privilege == null) {
-			String privilegeName = line.get(HEADER_PRIVILEGE_NAME, true);
-			if (privilegeName != null) {
-				privilege = service.getPrivilege(privilegeName);
-			}
+		if (privilegeName == null) {
+			throw new IllegalArgumentException(
+			        "A privilege must at least be provided a privilege name: '" + line.toString() + "'");
 		}
+		
+		Privilege privilege = service.getPrivilege(privilegeName);
 		if (privilege == null) {
 			privilege = new Privilege();
-			if (!StringUtils.isEmpty(uuid)) {
-				privilege.setUuid(uuid);
-			}
 		}
-		
 		return privilege;
 	}
 	
 	@Override
 	protected Privilege fill(Privilege privilege, CsvLine line) throws IllegalArgumentException {
-		
-		String privilegeName = line.get(HEADER_PRIVILEGE_NAME, true);
-		if (privilegeName == null) {
-			throw new IllegalArgumentException(
-			        "A privilege must at least be provided a privilege name: '" + line.toString() + "'");
-		}
-		if (privilege.getName() != null && !privilege.getName().equals(privilegeName)) {
-			throw new IllegalArgumentException("A privilege name cannot be edited: '" + line.toString() + "'");
-		}
-		privilege.setName(privilegeName);
-		privilege.setPrivilege(privilegeName);
-		
-		String privilegeDesc = line.get(HEADER_DESC, true);
-		privilege.setDescription(privilegeDesc);
-		
+		privilege.setPrivilege(line.get(HEADER_PRIVILEGE_NAME, true));
+		privilege.setName(privilege.getPrivilege());
+		privilege.setDescription(line.get(HEADER_DESC));
 		return privilege;
 	}
 }
--- api/src/test/java/org/openmrs/module/initializer/api/PrivilegesLoaderIntegrationTest.java
+++ api/src/test/java/org/openmrs/module/initializer/api/PrivilegesLoaderIntegrationTest.java
@@ -22,25 +22,11 @@ public class PrivilegesLoaderIntegrationTest extends DomainBaseModuleContextSens
 	@Before
 	public void setup() {
 		
-		// privileges to be edited (description)
-		{
-			Privilege priv = new Privilege();
-			priv.setUuid("cf68b952-2700-102b-80cb-0017a47871b2");
-			priv.setPrivilege("Edit Concepts");
-			priv.setDescription("Test description");
-			us.savePrivilege(priv);
-		}
+		// privilege to be edited
 		{
 			Privilege priv = new Privilege();
 			priv.setPrivilege("Add People");
-			priv.setDescription("Add People objects");
-			us.savePrivilege(priv);
-		}
-		// A privilege not to be edited
-		{
-			Privilege priv = new Privilege();
-			priv.setUuid("cf687ee2-2700-102b-80cb-0017a47871b2");
-			priv.setPrivilege("Add Test Privilege");
+			priv.setDescription("Able to add folks.");
 			us.savePrivilege(priv);
 		}
 	}
@@ -51,46 +37,21 @@ public class PrivilegesLoaderIntegrationTest extends DomainBaseModuleContextSens
 		// Replay
 		loader.load();
 		
-		// created privileges
-		{
-			Privilege priv = us.getPrivilegeByUuid("494e1213-360d-45ac-877d-515993444290");
-			Assert.assertNotNull(priv);
-			Assert.assertEquals("Add Cohort", priv.getName());
-			Assert.assertEquals("Add Cohort", priv.getPrivilege());
-			Assert.assertEquals("Able to add a cohort to the system", priv.getDescription());
-			
-		}
+		// created privilege
 		{
 			Privilege priv = us.getPrivilege("Add Patient");
 			Assert.assertNotNull(priv);
 			Assert.assertEquals("Add Patient", priv.getName());
-			Assert.assertEquals("Able to add new patients to the system", priv.getDescription());
+			Assert.assertEquals(priv.getName(), priv.getPrivilege());
+			Assert.assertEquals("Able to add patients.", priv.getDescription());
 		}
-		// edited privileges
-		{
-			Privilege priv = us.getPrivilegeByUuid("cf68b952-2700-102b-80cb-0017a47871b2");
-			Assert.assertNotNull(priv);
-			Assert.assertEquals("Edit Concepts", priv.getName());
-			Assert.assertEquals("Edit Concepts", priv.getPrivilege());
-			Assert.assertEquals("Able to change attributes of existing terms", priv.getDescription());
-		}
+		// edited privilege
 		{
 			Privilege priv = us.getPrivilege("Add People");
 			Assert.assertNotNull(priv);
 			Assert.assertEquals("Add People", priv.getName());
-			Assert.assertEquals("Able to add person objects", priv.getDescription());
-		}
-		// CSV line without name should result in no privilege creation
-		{
-			Privilege priv = us.getPrivilegeByUuid("cf688144-2700-102b-80cb-0017a47871b2");
-			Assert.assertNull(priv);
-		}
-		// a privilege name cannot be edited
-		{
-			Privilege priv = us.getPrivilegeByUuid("cf687ee2-2700-102b-80cb-0017a47871b2");
-			Assert.assertNotNull(priv);
-			Assert.assertEquals("Add Test Privilege", priv.getName());
-			Assert.assertEquals("Add Test Privilege", priv.getPrivilege());
+			Assert.assertEquals(priv.getName(), priv.getPrivilege());
+			Assert.assertEquals("Able to add people.", priv.getDescription());
 		}
 	}
 }
