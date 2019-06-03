87bc33f67d20e654f6fef7aad1d132d96b538807
mks-d <dimitri@mekomsolutions.com>
2019-06-03 17:32:23 +0200

    OPENMRS-83: Simplifying InitializerLogFactory + adding JavaDocs.
      modified:   api/src/main/java/org/openmrs/module/initializer/InitializerLogFactory.java

--- api/src/main/java/org/openmrs/module/initializer/InitializerLogFactory.java
+++ api/src/main/java/org/openmrs/module/initializer/InitializerLogFactory.java
@@ -2,8 +2,6 @@ package org.openmrs.module.initializer;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,12 +11,15 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 import org.openmrs.util.OpenmrsUtil;
 
+/**
+ * Custom logger that ensures that log messages are also routed through to the module's specific log
+ * file. Use this log factory instead of the usual {@link LogFactory} when custom logging needs to
+ * be enabled for a class.
+ */
 public class InitializerLogFactory {
 	
 	private static final String inizLogFilePath = Paths
 	        .get(OpenmrsUtil.getApplicationDataDirectory(), InitializerConstants.MODULE_ARTIFACT_ID + ".log").toString();
-	
-	private static Set<Class> classesWithLogger = new HashSet<Class>();
 	
 	@SuppressWarnings("rawtypes")
 	public static Log getLog(Class className) {
@@ -27,11 +28,9 @@ public class InitializerLogFactory {
 		
 		final Logger logger = Logger.getLogger(className);
 		try {
-			if (!classesWithLogger.contains(className)) {
-				logger.addAppender(
-				    new FileAppender(new PatternLayout("%p - %C{1}.%M(%L) |%d{ISO8601}| %m%n"), inizLogFilePath, true));
-				logger.setLevel((Level) Level.ALL);
-			}
+			logger.addAppender(
+			    new FileAppender(new PatternLayout("%p - %C{1}.%M(%L) |%d{ISO8601}| %m%n"), inizLogFilePath, true));
+			logger.setLevel((Level) Level.ALL);
 		}
 		catch (IOException e) {
 			log.error("The custom logger could not be setup, defaulting on the usual logging mechanism.", e);
