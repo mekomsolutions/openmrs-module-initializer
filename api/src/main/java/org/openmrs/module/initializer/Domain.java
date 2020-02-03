9340da059cbc4ad3654082ed6662430d424c0522
Brandon Istenes <bistenes@gmail.com>
2020-02-03 09:33:25 -0500

    Format

--- api/src/main/java/org/openmrs/module/initializer/Domain.java
+++ api/src/main/java/org/openmrs/module/initializer/Domain.java
@@ -1,15 +1,15 @@
 package org.openmrs.module.initializer;
 
 public enum Domain {
-
+	
 	JSON_KEY_VALUES(1, "jsonkeyvalues"), MDS(2, "metadatasharing"), PATIENT_IDENTIFIER_TYPES(3,
 	        "patientidentifiertypes"), METADATA_MAPPINGS(4, "metadatamappings"), PRIVILEGES(5,
 	                "privileges"), ENCOUNTER_TYPES(6, "encountertypes"), ROLES(7, "roles"), GLOBAL_PROPERTIES(8,
 	                        "globalproperties"), ATTRIBUTE_TYPES(9, "attributetypes"), LOCATIONS(10,
-	                                "locations"), CONCEPT_CLASSES(11,
-	                        "conceptclasses"), CONCEPTS(12, "concepts"), PROGRAMS(13, "programs"), PROGRAM_WORKFLOWS(14,
-	                                        "programworkflows"), PROGRAM_WORKFLOW_STATES(15,
-	                                                "programworkflowstates"), PERSON_ATTRIBUTE_TYPES(16,
+	                                "locations"), CONCEPT_CLASSES(11, "conceptclasses"), CONCEPTS(12, "concepts"), PROGRAMS(
+	                                        13,
+	                                        "programs"), PROGRAM_WORKFLOWS(14, "programworkflows"), PROGRAM_WORKFLOW_STATES(
+	                                                15, "programworkflowstates"), PERSON_ATTRIBUTE_TYPES(16,
 	                                                        "personattributetypes"), IDENTIFIER_SOURCES(17,
 	                                                                "idgen"), DRUGS(18, "drugs"), ORDER_FREQUENCIES(19,
 	                                                                        "orderfrequencies"), ORDER_TYPES(20,
