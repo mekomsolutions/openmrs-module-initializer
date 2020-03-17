45f156d295a9c5ad503985adfd5b316cfb76aade
mks-d <dimitri@mekomsolutions.com>
2020-03-17 08:06:44 +0100

    OPENMRS-75: Simplifying AttributeTypeEntity enum.

--- api/src/main/java/org/openmrs/module/initializer/api/attributes/types/AttributeTypeEntity.java
+++ api/src/main/java/org/openmrs/module/initializer/api/attributes/types/AttributeTypeEntity.java
@@ -1,21 +1,9 @@
 package org.openmrs.module.initializer.api.attributes.types;
 
-import org.openmrs.LocationAttributeType;
-
 public enum AttributeTypeEntity {
-	LOCATION(LocationAttributeType.class.getSimpleName()),
-	VISIT(LocationAttributeType.class.getSimpleName()),
-	CONCEPT(LocationAttributeType.class.getSimpleName()),
-	PROVIDER(LocationAttributeType.class.getSimpleName()),
-	PROGRAM(LocationAttributeType.class.getSimpleName());
-	
-	private final String attTypeName;
-	
-	AttributeTypeEntity(String attTypeName) {
-		this.attTypeName = attTypeName;
-	}
-	
-	public String getName() {
-		return attTypeName;
-	}
+	LOCATION,
+	VISIT,
+	CONCEPT,
+	PROVIDER,
+	PROGRAM;
 }
