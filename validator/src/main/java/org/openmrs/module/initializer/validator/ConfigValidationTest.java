bd2f2e8006cfbab58693f6a34298ebe608aefaef
mks-d <dimitri@mekomsolutions.com>
2020-11-04 21:23:49 +0100

    Added validator Maven module with app running a test class.

---
+++ validator/src/main/java/org/openmrs/module/initializer/validator/ConfigValidationTest.java
@@ -0,0 +1,12 @@
+package org.openmrs.module.initializer.validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConfigValidationTest {
+	
+	@Test
+	public void should() {
+		Assert.assertTrue(true);
+	}
+}
---
+++ validator/src/main/java/org/openmrs/module/initializer/validator/Validator.java
@@ -0,0 +1,14 @@
+package org.openmrs.module.initializer.validator;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+public class Validator {
+	
+	public static void main(String[] args) {
+		JUnitCore junit = new JUnitCore();
+		Result result = junit.run(ConfigValidationTest.class);
+		
+		System.out.println(result.wasSuccessful());
+	}
+}
