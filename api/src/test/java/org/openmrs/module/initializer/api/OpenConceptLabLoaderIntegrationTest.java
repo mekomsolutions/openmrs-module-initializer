9267ef077aba4b7ab6ec69e4d0bcba1bd29a06eb
Ian <ian_bacher@brown.edu>
2021-03-29 10:35:25 -0400

    Minor fixes

--- api/src/test/java/org/openmrs/module/initializer/api/OpenConceptLabLoaderIntegrationTest.java
+++ api/src/test/java/org/openmrs/module/initializer/api/OpenConceptLabLoaderIntegrationTest.java
@@ -10,38 +10,56 @@
 package org.openmrs.module.initializer.api;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.DaemonToken;
+import org.openmrs.module.ModuleFactory;
 import org.openmrs.module.initializer.DomainBaseModuleContextSensitiveTest;
 import org.openmrs.module.initializer.api.loaders.OpenConceptLabLoader;
+import org.openmrs.module.openconceptlab.OpenConceptLabActivator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.openmrs.module.openconceptlab.Utils;
-import java.util.zip.ZipFile;
-import java.io.IOException;
-import java.io.InputStream;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 import org.openmrs.Concept;
 
 public class OpenConceptLabLoaderIntegrationTest extends DomainBaseModuleContextSensitiveTest {
 	
 	@Autowired
 	private OpenConceptLabLoader loader;
+
+	@SuppressWarnings("unchecked")
+	@BeforeClass
+	public static void setupDaemonToken() {
+		Map<String, DaemonToken> daemonTokens;
+		try {
+			Field field = ModuleFactory.class.getDeclaredField("daemonTokens");
+			field.setAccessible(true);
+			daemonTokens = (Map<String, DaemonToken>) field.get(null);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		DaemonToken daemonToken = new DaemonToken("openconceptlab");
+		daemonTokens.put(daemonToken.getId(), daemonToken);
+		new OpenConceptLabActivator().setDaemonToken(daemonToken);
+	}
 	
 	@Test
-	public void load_shouldImportOCLPackages() throws IOException {
-		
-		Utils utils = null;
-		ZipFile zipFile = new ZipFile(
-		        "src/test/resources/testAppDataDir/configuration/openconceptlab/CIEL_v2021-01-29.20210204075515.zip");
-		InputStream is = utils.extractExportInputStreamFromZip(zipFile);
-		
+	public void load_shouldImportOCLPackages() {
+		{
+			Concept c = Context.getConceptService().getConceptByUuid("4421da0d-42d0-410d-8ffd-47ec6f155d8f");
+			Assert.assertNull(c);
+		}
+
 		loader.load();
 		
 		{
-		Concept c = null;
-		c = Context.getConceptService().getConceptByUuid("4421da0d-42d0-410d-8ffd-47ec6f155d8f");
-		Assert.assertEquals(2, c.getSetMembers().size());
-		Assert.assertTrue(c.isSet());
+			Concept c  = Context.getConceptService().getConceptByUuid("4421da0d-42d0-410d-8ffd-47ec6f155d8f");
+			Assert.assertEquals(2, c.getSetMembers().size());
+			Assert.assertTrue(c.getSet());
 		}
-		
 	}
 }
