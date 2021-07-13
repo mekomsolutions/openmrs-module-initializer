df859955676052a5a38a1c1a1c7ef1d8fad1b528
ruhanga <ruhanganathan@gmail.com>
2021-07-13 11:38:00 +0300

    Issue-95: Adding formatting changes

--- api/src/test/java/org/openmrs/module/initializer/api/BaseI18nSupportLoaderIntegrationTest.java
+++ api/src/test/java/org/openmrs/module/initializer/api/BaseI18nSupportLoaderIntegrationTest.java
@@ -21,6 +21,7 @@ public class BaseI18nSupportLoaderIntegrationTest extends DomainBaseModuleContex
 	
 	@Autowired
 	private InitializerService iniz;
+	
 	@Autowired
 	@Qualifier("encounterService")
 	private EncounterService es;
@@ -42,7 +43,8 @@ public class BaseI18nSupportLoaderIntegrationTest extends DomainBaseModuleContex
 		
 		// Testing on EncounterTypes and Locations domains
 		for (Loader loader : iniz.getLoaders()) {
-			if (loader.getDomainName().equals(Domain.ENCOUNTER_TYPES.getName()) || loader.getDomainName().equals(Domain.LOCATIONS.getName())) {
+			if (loader.getDomainName().equals(Domain.ENCOUNTER_TYPES.getName())
+			        || loader.getDomainName().equals(Domain.LOCATIONS.getName())) {
 				loader.load();
 			}
 		}
@@ -57,25 +59,41 @@ public class BaseI18nSupportLoaderIntegrationTest extends DomainBaseModuleContex
 		String locUuid = loc.getUuid();
 		
 		// verify EncounterTypes domain i18n
-		Assert.assertEquals("Triage Encounter (translated)", Context.getMessageSourceService().getMessage("ui.i18n.EncounterType.name." + etUuid, null, localeEn));
-		Assert.assertEquals("ទ្រីយ៉ាហ្គេនស៊ើរ", Context.getMessageSourceService().getMessage("ui.i18n.EncounterType.name." + etUuid, null, localeKm));
-		Assert.assertEquals("Medical History Encounter (translated)", Context.getMessageSourceService().getMessage("ui.i18n.EncounterType.name.aaa1a367-3047-4833-af27-b30e2dac9028", null, localeEn));
-		Assert.assertEquals("ប្រវត្តិសាស្រ្តវេជ្ជសាស្រ្ត", Context.getMessageSourceService().getMessage("ui.i18n.EncounterType.name.aaa1a367-3047-4833-af27-b30e2dac9028", null, localeKm));
+		Assert.assertEquals("Triage Encounter (translated)",
+		    Context.getMessageSourceService().getMessage("ui.i18n.EncounterType.name." + etUuid, null, localeEn));
+		Assert.assertEquals("ទ្រីយ៉ាហ្គេនស៊ើរ",
+		    Context.getMessageSourceService().getMessage("ui.i18n.EncounterType.name." + etUuid, null, localeKm));
+		Assert.assertEquals("Medical History Encounter (translated)", Context.getMessageSourceService()
+		        .getMessage("ui.i18n.EncounterType.name.aaa1a367-3047-4833-af27-b30e2dac9028", null, localeEn));
+		Assert.assertEquals("ប្រវត្តិសាស្រ្តវេជ្ជសាស្រ្ត", Context.getMessageSourceService()
+		        .getMessage("ui.i18n.EncounterType.name.aaa1a367-3047-4833-af27-b30e2dac9028", null, localeKm));
 		
-		Assert.assertEquals("Triage Encounter (translated)", Context.getMessageSourceService().getMessage("org.openmrs.EncounterType." + etUuid, null, localeEn));
-		Assert.assertEquals("ទ្រីយ៉ាហ្គេនស៊ើរ", Context.getMessageSourceService().getMessage("org.openmrs.EncounterType." + etUuid, null, localeKm));
-		Assert.assertEquals("Medical History Encounter (translated)", Context.getMessageSourceService().getMessage("org.openmrs.EncounterType.aaa1a367-3047-4833-af27-b30e2dac9028", null, localeEn));
-		Assert.assertEquals("ប្រវត្តិសាស្រ្តវេជ្ជសាស្រ្ត", Context.getMessageSourceService().getMessage("org.openmrs.EncounterType.aaa1a367-3047-4833-af27-b30e2dac9028", null, localeKm));
+		Assert.assertEquals("Triage Encounter (translated)",
+		    Context.getMessageSourceService().getMessage("org.openmrs.EncounterType." + etUuid, null, localeEn));
+		Assert.assertEquals("ទ្រីយ៉ាហ្គេនស៊ើរ",
+		    Context.getMessageSourceService().getMessage("org.openmrs.EncounterType." + etUuid, null, localeKm));
+		Assert.assertEquals("Medical History Encounter (translated)", Context.getMessageSourceService()
+		        .getMessage("org.openmrs.EncounterType.aaa1a367-3047-4833-af27-b30e2dac9028", null, localeEn));
+		Assert.assertEquals("ប្រវត្តិសាស្រ្តវេជ្ជសាស្រ្ត", Context.getMessageSourceService()
+		        .getMessage("org.openmrs.EncounterType.aaa1a367-3047-4833-af27-b30e2dac9028", null, localeKm));
 		
 		// verify Locations domain i18n
-		Assert.assertEquals("The Lake Clinic-Cambodia (translated)", Context.getMessageSourceService().getMessage("ui.i18n.Location.name." + locUuid, null, localeEn));
-		Assert.assertEquals("គ្លីនីកគ្លីនិក - ប្រទេសកម្ពុជា", Context.getMessageSourceService().getMessage("ui.i18n.Location.name." + locUuid, null, localeKm));
-		Assert.assertEquals("Acme Clinic (translated)", Context.getMessageSourceService().getMessage("ui.i18n.Location.name.a03e395c-b881-49b7-b6fc-983f6bddc7fc", null, localeEn));
-		Assert.assertEquals("គ្លីនិកអាមី", Context.getMessageSourceService().getMessage("ui.i18n.Location.name.a03e395c-b881-49b7-b6fc-983f6bddc7fc", null, localeKm));
+		Assert.assertEquals("The Lake Clinic-Cambodia (translated)",
+		    Context.getMessageSourceService().getMessage("ui.i18n.Location.name." + locUuid, null, localeEn));
+		Assert.assertEquals("គ្លីនីកគ្លីនិក - ប្រទេសកម្ពុជា",
+		    Context.getMessageSourceService().getMessage("ui.i18n.Location.name." + locUuid, null, localeKm));
+		Assert.assertEquals("Acme Clinic (translated)", Context.getMessageSourceService()
+		        .getMessage("ui.i18n.Location.name.a03e395c-b881-49b7-b6fc-983f6bddc7fc", null, localeEn));
+		Assert.assertEquals("គ្លីនិកអាមី", Context.getMessageSourceService()
+		        .getMessage("ui.i18n.Location.name.a03e395c-b881-49b7-b6fc-983f6bddc7fc", null, localeKm));
 		
-		Assert.assertEquals("The Lake Clinic-Cambodia (translated)", Context.getMessageSourceService().getMessage("org.openmrs.Location." + locUuid, null, localeEn));
-		Assert.assertEquals("គ្លីនីកគ្លីនិក - ប្រទេសកម្ពុជា", Context.getMessageSourceService().getMessage("org.openmrs.Location." + locUuid, null, localeKm));
-		Assert.assertEquals("Acme Clinic (translated)", Context.getMessageSourceService().getMessage("org.openmrs.Location.a03e395c-b881-49b7-b6fc-983f6bddc7fc", null, localeEn));
-		Assert.assertEquals("គ្លីនិកអាមី", Context.getMessageSourceService().getMessage("org.openmrs.Location.a03e395c-b881-49b7-b6fc-983f6bddc7fc", null, localeKm));
+		Assert.assertEquals("The Lake Clinic-Cambodia (translated)",
+		    Context.getMessageSourceService().getMessage("org.openmrs.Location." + locUuid, null, localeEn));
+		Assert.assertEquals("គ្លីនីកគ្លីនិក - ប្រទេសកម្ពុជា",
+		    Context.getMessageSourceService().getMessage("org.openmrs.Location." + locUuid, null, localeKm));
+		Assert.assertEquals("Acme Clinic (translated)", Context.getMessageSourceService()
+		        .getMessage("org.openmrs.Location.a03e395c-b881-49b7-b6fc-983f6bddc7fc", null, localeEn));
+		Assert.assertEquals("គ្លីនិកអាមី", Context.getMessageSourceService()
+		        .getMessage("org.openmrs.Location.a03e395c-b881-49b7-b6fc-983f6bddc7fc", null, localeKm));
 	}
 }
