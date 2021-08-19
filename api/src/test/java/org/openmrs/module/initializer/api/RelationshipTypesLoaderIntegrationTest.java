0e2a1e7307245c4ee37c0ed21d802c901e0a24cc
Brandon Istenes <bistenes@gmail.com>
2021-08-19 09:01:24 -0700

    Review feedback

--- api/src/test/java/org/openmrs/module/initializer/api/RelationshipTypesLoaderIntegrationTest.java
+++ api/src/test/java/org/openmrs/module/initializer/api/RelationshipTypesLoaderIntegrationTest.java
@@ -19,34 +19,6 @@ public class RelationshipTypesLoaderIntegrationTest extends DomainBaseModuleCont
 	@Autowired
 	private RelationshipTypesLoader loader;
 	
-	private static final String RELATIONSHIP_TYPE_TO_EDIT_UUID = "53d8a8f3-0084-4a52-8666-c655f5bd2689";
-	
-	private static final String RELATIONSHIP_TYPE_TO_RETIRE_UUID = "3982f469-cedc-4b2d-91ea-fe38f881e1a0";
-	
-	@Before
-	public void setup() {
-		// a relationship type for retirement
-		{
-			RelationshipType rt = new RelationshipType();
-			rt.setName("Aunt/Niece");
-			rt.setDescription("A relationship of an aunt and her niece");
-			rt.setUuid(RELATIONSHIP_TYPE_TO_RETIRE_UUID);
-			rt.setaIsToB("Aunt");
-			rt.setbIsToA("Niece");
-			ps.saveRelationshipType(rt);
-		}
-		// a relationship type for edit
-		{
-			RelationshipType rt = new RelationshipType();
-			rt.setName("Supervisor/Supervisee");
-			rt.setDescription("An old description for supervisor to supervisee relationship");
-			rt.setUuid(RELATIONSHIP_TYPE_TO_EDIT_UUID);
-			rt.setaIsToB("Supervisor");
-			rt.setbIsToA("Supervisee");
-			ps.saveRelationshipType(rt);
-		}
-	}
-	
 	@Test
 	public void load_shouldLoadRelationshipTypesAccordingToCsvFiles() {
 		
@@ -67,7 +39,7 @@ public class RelationshipTypesLoaderIntegrationTest extends DomainBaseModuleCont
 		
 		// verify edited
 		{
-			RelationshipType rt = ps.getRelationshipTypeByUuid(RELATIONSHIP_TYPE_TO_EDIT_UUID);
+			RelationshipType rt = ps.getRelationshipTypeByUuid("53d8a8f3-0084-4a52-8666-c655f5bd2689");
 			Assert.assertNotNull(rt);
 			Assert.assertEquals("Supervisor/Supervisee", rt.getName());
 			Assert.assertEquals("A new description for supervisor to supervisee relationship", rt.getDescription());
@@ -75,7 +47,7 @@ public class RelationshipTypesLoaderIntegrationTest extends DomainBaseModuleCont
 		
 		// verify retired
 		{
-			RelationshipType rt = ps.getRelationshipTypeByUuid(RELATIONSHIP_TYPE_TO_RETIRE_UUID);
+			RelationshipType rt = ps.getRelationshipTypeByUuid("3982f469-cedc-4b2d-91ea-fe38f881e1a0");
 			Assert.assertNotNull(rt);
 			Assert.assertTrue(rt.getRetired());
 		}
