ca0e0616cc1364f815d74f6608363dd958d281d7
ruhanga <ruhanganathan@gmail.com>
2022-12-13 13:34:01 +0300

    issues-180: Depending on FormResource

--- api/src/main/java/org/openmrs/module/initializer/api/loaders/AmpathFormsTranslationsLoader.java
+++ api/src/main/java/org/openmrs/module/initializer/api/loaders/AmpathFormsTranslationsLoader.java
@@ -3,10 +3,13 @@ package org.openmrs.module.initializer.api.loaders;
 import java.io.File;
 import java.util.Map;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.openmrs.api.DatatypeService;
-import org.openmrs.api.db.ClobDatatypeStorage;
+import org.openmrs.Form;
+import org.openmrs.FormResource;
+import org.openmrs.api.FormService;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.initializer.Domain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,7 +19,7 @@ import java.nio.charset.StandardCharsets;
 public class AmpathFormsTranslationsLoader extends BaseFileLoader {
 	
 	@Autowired
-	private DatatypeService datatypeService;
+	private FormService formService;
 	
 	@Override
 	protected Domain getDomain() {
@@ -31,25 +34,43 @@ public class AmpathFormsTranslationsLoader extends BaseFileLoader {
 	@Override
 	protected void load(File file) throws Exception {
 		String jsonTranslationsString = FileUtils.readFileToString(file, StandardCharsets.UTF_8.toString());
-		Map<String, Object> jsonTranslationsFile = new ObjectMapper().readValue(jsonTranslationsString, Map.class);
+		Map<String, Object> jsonTranslationsDefinition = new ObjectMapper().readValue(jsonTranslationsString, Map.class);
 		
-		String clobUuid = (String) jsonTranslationsFile.get("uuid");
-		if (StringUtils.isBlank(clobUuid)) {
+		String formResourceUuid = (String) jsonTranslationsDefinition.get("uuid");
+		if (StringUtils.isBlank(formResourceUuid)) {
 			throw new Exception("Uuid is required for AMPATH forms translations loader");
 		}
 		
-		String language = (String) jsonTranslationsFile.get("language");
-		if (StringUtils.isBlank(language)) {
-			throw new Exception("Language is required for AMPATH forms translations loader");
+		String jsonTranlsationsFileName = FilenameUtils.removeExtension(file.getName());
+		
+		String language = (String) jsonTranslationsDefinition.get("language");
+		if (StringUtils.isBlank(language)
+		        && !language.equalsIgnoreCase(jsonTranlsationsFileName.split("_translations_")[1])) {
+			throw new Exception(
+			        "'language' is required for AMPATH forms translations loader and should align with locale appended to the file name. File names should be of the format '<form_name>_translations_<locale>'");
 		}
 		
-		ClobDatatypeStorage clobTranslationsData = datatypeService.getClobDatatypeStorageByUuid(clobUuid);
-		if (clobTranslationsData == null) {
-			clobTranslationsData = new ClobDatatypeStorage();
+		Form form = null;
+		FormResource formResource = Context.getFormService().getFormResourceByUuid(formResourceUuid);
+		if (formResource == null) {
+			formResource = new FormResource();
+			formResource.setUuid(formResourceUuid);
+		} else {
+			form = formResource.getForm();
 		}
 		
-		clobTranslationsData.setUuid(clobUuid);
-		clobTranslationsData.setValue(jsonTranslationsString);
-		datatypeService.saveClobDatatypeStorage(clobTranslationsData);
+		if (form == null) {
+			form = formService.getForm(jsonTranlsationsFileName.split("_translations_")[0]);
+			if (form == null) {
+				throw new RuntimeException("No AMPATH form exists for AMPATH form tranlsations file: " + file.getName()
+				        + ". File names should be of the format '<form_name>_translations_<locale>'");
+			}
+		}
+		
+		formResource.setForm(form);
+		formResource.setName(form.getName() + "_translations_" + language);
+		formResource.setDatatypeClassname("org.openmrs.customdatatype.datatype.LongFreeTextDatatype");
+		formResource.setValue(jsonTranslationsString);
+		formService.saveFormResource(formResource);
 	}
 }
