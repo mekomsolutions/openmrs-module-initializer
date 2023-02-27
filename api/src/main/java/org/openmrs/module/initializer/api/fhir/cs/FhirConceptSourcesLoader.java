2971375dc4ac15ba49e765aca85c5046e57ff7a6
Ian <52504170+ibacher@users.noreply.github.com>
2023-02-27 08:35:27 -0500

    Fix FHIR domains to properly use @OpenmrsProfile (#222)

--- api/src/main/java/org/openmrs/module/initializer/api/fhir/cs/FhirConceptSourcesLoader.java
+++ api/src/main/java/org/openmrs/module/initializer/api/fhir/cs/FhirConceptSourcesLoader.java
@@ -4,10 +4,8 @@ import org.openmrs.annotation.OpenmrsProfile;
 import org.openmrs.module.fhir2.model.FhirConceptSource;
 import org.openmrs.module.initializer.api.loaders.BaseCsvLoader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
-@OpenmrsProfile(modules = { "fhir2:1.6.*" })
+@OpenmrsProfile(modules = { "fhir2:1.6.* - 9.*" })
 public class FhirConceptSourcesLoader extends BaseCsvLoader<FhirConceptSource, FhirConceptSourceCsvParser> {
 	
 	@Autowired
