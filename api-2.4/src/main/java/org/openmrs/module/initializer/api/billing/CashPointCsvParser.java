dc07b7679afcd3af67b501e8b32bd308be3a6368
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2024-06-10 17:00:35 +0300

    Add CashPoints Domain

---
+++ api-2.4/src/main/java/org/openmrs/module/initializer/api/billing/CashPointCsvParser.java
@@ -0,0 +1,48 @@
+package org.openmrs.module.initializer.api.billing;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openmrs.module.billing.api.ICashPointService;
+import org.openmrs.module.billing.api.model.CashPoint;
+import org.openmrs.module.initializer.Domain;
+import org.openmrs.module.initializer.api.BaseLineProcessor;
+import org.openmrs.module.initializer.api.CsvLine;
+import org.openmrs.module.initializer.api.CsvParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class CashPointCsvParser extends CsvParser<CashPoint, BaseLineProcessor<CashPoint>> {
+    
+    private final ICashPointService iCashPointService;
+    
+    @Autowired
+    public CashPointCsvParser(@Qualifier("iCashPointService") ICashPointService iCashPointService, CashPointsLineProcessor processor) {
+        super(processor);
+        this.iCashPointService = iCashPointService;
+    }
+    
+    @Override
+    public Domain getDomain() {
+        return Domain.CASH_POINTS;
+    }
+    
+    @Override
+    public CashPoint bootstrap(CsvLine line) throws IllegalArgumentException {
+        String uuid = line.getUuid();
+        CashPoint cashPoint = null;
+        if (StringUtils.isNotBlank(uuid)) {
+            cashPoint = iCashPointService.getByUuid(uuid);
+        }
+        if (cashPoint == null) {
+            cashPoint = new CashPoint();
+            if (StringUtils.isNotBlank(uuid)) {
+                cashPoint.setUuid(uuid);
+            }
+        }
+        return cashPoint;
+    }
+    
+    @Override
+    public CashPoint save(CashPoint instance) {
+        return iCashPointService.save(instance);
+    }
+}
---
+++ api-2.4/src/main/java/org/openmrs/module/initializer/api/billing/CashPointLoader.java
@@ -0,0 +1,14 @@
+package org.openmrs.module.initializer.api.billing;
+
+import org.openmrs.module.billing.api.model.CashPoint;
+import org.openmrs.module.initializer.api.loaders.BaseCsvLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CashPointLoader extends BaseCsvLoader<CashPoint, CashPointCsvParser> {
+	
+	@Autowired
+	public void setParser(CashPointCsvParser parser) {
+		this.parser = parser;
+	}
+    
+}
