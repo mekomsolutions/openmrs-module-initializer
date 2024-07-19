0f9777b858a8eb3a56f14119b1a44e1944c74788
ruhanga <ruhanganathan@gmail.com>
2024-07-19 13:30:15 +0300

    Improve tests

--- api-2.4/src/test/java/org/openmrs/module/initializer/api/BillableServiceLoaderIntegrationTest.java
+++ api-2.4/src/test/java/org/openmrs/module/initializer/api/BillableServiceLoaderIntegrationTest.java
@@ -10,7 +10,9 @@
 package org.openmrs.module.initializer.api;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openmrs.Concept;
 import org.openmrs.api.ConceptService;
 import org.openmrs.module.billing.api.IBillableItemsService;
 import org.openmrs.module.billing.api.model.BillableService;
@@ -20,67 +22,75 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 public class BillableServiceLoaderIntegrationTest extends DomainBaseModuleContextSensitive_2_4_test {
-
-    @Autowired
-    @Qualifier("conceptService")
-    private ConceptService conceptService;
-
-    @Autowired
-    private BillableServiceLoader loader;
-
-    @Autowired
-    private IBillableItemsService billableItemsService;
-
-    @Test
-    public void load_shouldLoadBillableServicesAccordingToCsvFiles() {
-        // Replay
-        loader.load();
-
-        // Verify fetch
-        {
-            BillableService service1 = billableItemsService.getByUuid("44ebd6cd-04ad-4eba-8ce1-0de4564bfd17");
-            Assert.assertNotNull(service1);
-            Assert.assertEquals(conceptService.getConceptByUuid("1592AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), service1.getConcept());
-            Assert.assertEquals(BillableServiceStatus.ENABLED, service1.getServiceStatus());
-
-            BillableService service2 = billableItemsService.getByUuid("a0f7d8a1-4fa2-418c-aa8a-9b358f43d605");
-            Assert.assertNotNull(service2);
-            Assert.assertEquals(conceptService.getConceptByUuid("164949AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), service2.getConcept());
-            Assert.assertEquals(BillableServiceStatus.ENABLED, service2.getServiceStatus());
-
-            BillableService service3 = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
-            Assert.assertNotNull(service3);
-            Assert.assertEquals("Nutrition counseling", service3.getName());
-            Assert.assertEquals(BillableServiceStatus.ENABLED, service3.getServiceStatus());
-        }
-
-        // Modify an existing entity in the CSV
-        {
-            BillableService service = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
-            service.setName("Nutrition counseling updated");
-            service.setServiceStatus(BillableServiceStatus.DISABLED);
-            billableItemsService.save(service);
-
-            BillableService updatedService = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
-            Assert.assertEquals("Nutrition counseling updated", updatedService.getName());
-            Assert.assertEquals(BillableServiceStatus.DISABLED, updatedService.getServiceStatus());
-        }
-
-        // Retire and un-retire an existing entity via CSV
-        {
-            BillableService service = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
-            service.setServiceStatus(BillableServiceStatus.DISABLED);
-            billableItemsService.save(service);
-
-            BillableService retiredService = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
-            Assert.assertEquals(BillableServiceStatus.DISABLED, retiredService.getServiceStatus());
-
-            // Unretire the entity
-            retiredService.setServiceStatus(BillableServiceStatus.ENABLED);
-            billableItemsService.save(retiredService);
-
-            BillableService unretiredService = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
-            Assert.assertEquals(BillableServiceStatus.ENABLED, unretiredService.getServiceStatus());
-        }
-    }
+	
+	@Autowired
+	@Qualifier("conceptService")
+	private ConceptService conceptService;
+	
+	@Autowired
+	private BillableServiceLoader loader;
+	
+	@Autowired
+	private IBillableItemsService billableItemsService;
+	
+	@Before
+	public void setup() {
+		executeDataSet("testdata/test-concepts-2.4.xml");
+		{
+			Concept concept = conceptService.getConceptByUuid("164949AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+			
+			BillableService service = new BillableService();
+			service.setUuid("a0f7d8a1-4fa2-418c-aa8a-9b358f43d605");
+			service.setName("Orthopedic Therapy");
+			service.setShortName("OTHS");
+			service.setConcept(concept);
+			service.setServiceStatus(BillableServiceStatus.ENABLED);
+			billableItemsService.save(service);
+		}
+	}
+	
+	@Test
+	public void load_shouldLoadBillableServicesAccordingToCsvFiles() {
+		// Replay
+		loader.load();
+		
+		// Verify creation
+		{
+			BillableService service = billableItemsService.getByUuid("44ebd6cd-04ad-4eba-8ce1-0de4564bfd17");
+			Assert.assertNotNull(service);
+			Assert.assertNotNull(service.getServiceType());
+			Assert.assertEquals(conceptService.getConceptByUuid("1592AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
+			    service.getConcept());
+			Assert.assertEquals(BillableServiceStatus.ENABLED, service.getServiceStatus());
+		}
+		
+		// Verify edition
+		{
+			BillableService service = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
+			service.setName("Nutrition counseling updated");
+			service.setServiceStatus(BillableServiceStatus.DISABLED);
+			billableItemsService.save(service);
+			
+			BillableService updatedService = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
+			Assert.assertEquals("Nutrition counseling updated", updatedService.getName());
+			Assert.assertEquals(BillableServiceStatus.DISABLED, updatedService.getServiceStatus());
+		}
+		
+		// Verify retirement
+		{
+			BillableService service = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
+			service.setServiceStatus(BillableServiceStatus.DISABLED);
+			billableItemsService.save(service);
+			
+			BillableService retiredService = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
+			Assert.assertEquals(BillableServiceStatus.DISABLED, retiredService.getServiceStatus());
+			
+			// Unretire the entity
+			retiredService.setServiceStatus(BillableServiceStatus.ENABLED);
+			billableItemsService.save(retiredService);
+			
+			BillableService unretiredService = billableItemsService.getByUuid("16435ab4-27c3-4d91-b21e-52819bd654d8");
+			Assert.assertEquals(BillableServiceStatus.ENABLED, unretiredService.getServiceStatus());
+		}
+	}
 }
