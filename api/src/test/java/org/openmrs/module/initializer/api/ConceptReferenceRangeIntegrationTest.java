cf86dc030535dfd5b99e0082742e07eeeaea25e7
Isaiah Muli <dickson.isaiah6@gmail.com>
2024-09-18 23:24:10 +0300

    Adding integration test

--- api/src/test/java/org/openmrs/module/initializer/api/ConceptReferenceRangeIntegrationTest.java
+++ api/src/test/java/org/openmrs/module/initializer/api/ConceptReferenceRangeIntegrationTest.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.openmrs.ConceptReferenceRange;
 import org.openmrs.api.ConceptService;
 import org.openmrs.module.initializer.DomainBaseModuleContextSensitiveTest;
 import org.openmrs.module.initializer.api.c.ConceptReferenceRangeLoader;
@@ -13,33 +14,27 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 public class ConceptReferenceRangeIntegrationTest extends DomainBaseModuleContextSensitiveTest {
 
-    @Autowired
-    private ConceptService conceptService;
+	@Autowired
+	private ConceptService conceptService;
 
-    @Autowired
-    private ConceptReferenceRangeLoader conceptReferenceRangeLoader;
+	@Autowired
+	private ConceptReferenceRangeLoader conceptReferenceRangeLoader;
 
-    @Autowired
-    @Qualifier("sessionFactory")
-    private SessionFactory sessionFactory;
+	@Autowired
+	@Qualifier("sessionFactory")
+	private SessionFactory sessionFactory;
 
-    @Test
-    public void load_shouldLoadConceptReferenceRangeFromCsvFiles() {
-        conceptReferenceRangeLoader.load();
+	@Test
+	public void load_shouldLoadConceptReferenceRangeFromCsvFiles() {
+		conceptReferenceRangeLoader.load();
 
-        Session session = sessionFactory.getCurrentSession();
-        Query getObsCategoryByCategoryQuery = session.createQuery("from " + ConceptReferenceRange.class.getSimpleName()
-                + " where observationCategory = :observationCategory");
+		Session session = sessionFactory.getCurrentSession();
 
-        {
-            ConceptReferenceRange referenceRange = conceptService.getConceptReferenceRangeByUuid("69d620da-93c4-4767-916e-48f5fe8824c4");
-            Assert.assertNotNull(referenceRange);
-            Assert.assertEquals("Reference range 1", referenceRange.getName());
-        }
-
-        {
-            conceptService.getConceptReferenceRangesByConcept();
-            assertThat(observationCategories, hasSize(1));
-        }
-    }
+		{
+			ConceptReferenceRange referenceRange = conceptService
+			        .getConceptReferenceRangeByUuid("69d620da-93c4-4767-916e-48f5fe8824c4");
+			Assert.assertNotNull(referenceRange);
+			Assert.assertEquals("Reference range 1", referenceRange.getCriteria());
+		}
+	}
 }
