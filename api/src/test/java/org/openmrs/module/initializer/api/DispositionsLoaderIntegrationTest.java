29d53bbac8f9c895ef31da9d1065b4f8b7d34021
mogoodrich <mgoodrich@pih.org>
2024-10-28 13:33:05 -0400

    [#284] - Support "dispositions" domain

--- api/src/test/java/org/openmrs/module/initializer/api/DispositionsLoaderIntegrationTest.java
+++ api/src/test/java/org/openmrs/module/initializer/api/DispositionsLoaderIntegrationTest.java
@@ -1,10 +1,13 @@
 package org.openmrs.module.initializer.api;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.openmrs.module.emrapi.disposition.DispositionService;
 import org.openmrs.module.initializer.DomainBaseModuleContextSensitiveTest;
 import org.openmrs.module.initializer.api.loaders.DispositionsLoader;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -37,8 +40,18 @@ public class DispositionsLoaderIntegrationTest extends DomainBaseModuleContextSe
 	}
 	
 	@Test(expected = RuntimeException.class)
-	public void load_shouldThrowExceptionInUnsafeMode() throws Exception {
-		loader.loadUnsafe(null, true);
+	public void load_shouldThrowExceptionInUnsafeModeIfMultipleFiles() throws Exception {
+		String existingFilePath = loader.getDirUtil().getDomainDirPath() + "/dispositionConfig.json";
+		String additionalFilePath = loader.getDirUtil().getDomainDirPath() + "/additionalDispositionConfig.json";
+		File srcFile = new File(existingFilePath);
+		File dstFile = new File(additionalFilePath);
+		FileUtils.copyFile(srcFile, dstFile);
+
+		try {
+			loader.loadUnsafe(null, true);
+		}
+		finally {
+			FileUtils.deleteQuietly(dstFile);
+		}
 	}
-	
 }
